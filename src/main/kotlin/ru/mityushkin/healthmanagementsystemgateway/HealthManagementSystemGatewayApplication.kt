51aa28305532d688dd8f4b5f4277c0b59714069b
Mityushkin <ndmityushkin@sberbank.ru>
2024-04-20 14:57:34 +0700

    init service

---
+++ src/main/kotlin/ru/mityushkin/healthmanagementsystemgateway/HealthManagementSystemGatewayApplication.kt
@@ -0,0 +1,11 @@
+package ru.mityushkin.healthmanagementsystemgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HealthManagementSystemGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<HealthManagementSystemGatewayApplication>(*args)
+}
---
+++ src/test/kotlin/ru/mityushkin/healthmanagementsystemgateway/HealthManagementSystemGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.mityushkin.healthmanagementsystemgateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HealthManagementSystemGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
